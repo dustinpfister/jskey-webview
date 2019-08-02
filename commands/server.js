76574f1b3ddbff1dd469ee8fae308d46ae4308f5
dustin pfister <dustin.pfister@gmail.com>
2019-08-02 08:56:04 -0400

    port option for server command

--- commands/server.js
+++ commands/server.js
@@ -1,8 +1,15 @@
-let pack = require('../package.json'),
+let express = require('express'),
 path = require('path');
 
 exports.command = 'server';
 exports.describe = 'server command';
+exports.builder = {
+    // port
+    p: {
+      default: process.env.PORT || 8080
+    }
+};
 exports.handler = function (argv) {
     console.log('> jskey-webview: server command:');
+    console.log(argv.p)
 };
