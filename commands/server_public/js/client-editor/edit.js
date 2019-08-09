4f46860f2619f3292fdc2876c0a8bf41d9645ebf
dustin pfister <dustin.pfister@gmail.com>
2019-08-09 09:11:45 -0400

    changed style of editor

--- commands/server_public/js/client-editor/edit.js
+++ commands/server_public/js/client-editor/edit.js
@@ -2,21 +2,26 @@
 var Editor = new Vue({
   el: '#editor',
   template: '<div v-bind:style="constyle" >'+
-        '<div>fileName: {{ fileName }}</div>'+
-        '<div>Posts: </div>'+
-        '<div v-for="(post, index) in posts"><p v-on:click="onPostLink" >{{post}}</p></div>'+
-        '<textarea v-model="text" ></textarea>' +
-        '<input type="button" value="save" v-on:click="saveFile">'+
+        '<div v-bind:style="listStyle" ><h3>Posts List: </h3> '+
+            '<div v-for="(post, index) in posts"><p v-on:click="onPostLink" >{{post}}</p></div>'+
+        '</div>' +
+        '<div v-bind:style="editorStyle">' +
+            '<div>fileName: {{ fileName }}</div>'+
+            '<textarea v-model="text" rows="20" cols="80"></textarea><br>' +
+            '<input type="button" value="save" v-on:click="saveFile">'+
+        '</div>' +
     '</div>',
   data : {
-     constyle:'background:red;width:200px;padding:5px;',
-     posts : [],
-     text: 'hi there',
-     fileName: 'new_post.md'
+      constyle:'background:#bf0000;padding:5px;',
+      listStyle: 'background:blue;width:200px;padding:5px;display:inline-block;vertical-align: top;',
+      editorStyle: 'background:green;padding:5px;display:inline-block;',
+      posts : [],
+      text: 'hi there',
+      fileName: 'new_post.md'
   },
   methods:{
   
-     // get list of file names
+    // get list of file names
      getList : function(){
         this.$http.get('post_list')
         .then(function(res){
