e0197e3622585a9b481f43d996bef29f94b2dc52
dustin pfister <dustin.pfister@gmail.com>
2019-08-08 20:30:18 -0400

    crypting client body text

--- commands/server_middleware/post_save/index.js
+++ commands/server_middleware/post_save/index.js
@@ -14,32 +14,59 @@ module.exports = function(key){
     key.password = key.password || ''; // the password to be used with jskey-crypt
     key.random = key.random || ''; // the salt value to be used with jskey-crypt
     
-    router.post('/', [(req, res, next) => {
-        
-        // state
-        req.state = {
-            key : key,
-            dir_target : req.app.get('dir_target')
-        };
-        req.state.dir_posts_crypt = path.join(req.state.dir_target, '_posts_crypt');
+    router.post('/', [
     
-        res.resObj = {
-            success: false,
-        };
+        // create state and responseObj
+        (req, res, next) => {
         
-        next();
+            // state
+            req.state = {
+                key : key,
+                dir_target : req.app.get('dir_target')
+            };
+            req.state.dir_posts_crypt = path.join(req.state.dir_target, '_posts_crypt');
     
-    },
+            res.resObj = {
+                success: false
+            };
+        
+            next();
+    
+        },
+        
+        // crypt response body
+        (req, res, next) => {
+            
+            let key = req.state.key;
+            let crypt = spawn('jskey-crypt', ['pipe', '-c', '-p', key.password, '-s', key.random]);
+             
+            let text = '';
+            crypt.stdout.on('data', (data)=>{
+                text += data.toString();
+                // !!! should not be done here
+                // but it seems to work okay for now
+                crypt.stdin.end();
+            });
+            
+            // send text when
+            crypt.on('close', ()=>{
+                res.success = true;
+                res.resObj.mess = text;
+                next(); 
+            });
+            
+             crypt.stdin.write(req.body.text,'utf8');
+            
+ 
+        },
+        
         (req,res)=>{
             
             
-        res.json(res.resObj);
+            res.json(res.resObj);
             
         }
-    ]
-        
-        
-    );
+    ]);
 
     // return the router to be used
     // in main server.js file
