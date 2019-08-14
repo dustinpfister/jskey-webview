77ab5d0f3965c98847ec8c9d0a3cfb3cdd2bbd2c
dustin pfister <dustin.pfister@gmail.com>
2019-08-14 10:22:28 -0400

    checking resObj

--- commands/server_middleware/kwdb_index/index.js
+++ commands/server_middleware/kwdb_index/index.js
@@ -8,23 +8,64 @@ router.use(require('body-parser').json());
 
 module.exports = function(){
     
-    router.get('/', (req, res, next) => {
+    router.get('/', [
+       
+       // set up resObj
+       (req, res, next) => {
         
-        let dir_target = req.app.get('dir_target'),
-        path_kwdb = path.join(dir_target, '_kwdb', req.body.dbName + '.json');
-                
-        res.resObj = {
-            success : false,
-            mess: 'default message',
-            mode : req.query.m || 'post', // for a post or keyword
-            post: req.query.p || false,
-            keyword: req.query.k || false
-        };
+           let dir_target = req.app.get('dir_target'),
+           path_kwdb = path.join(dir_target, '_kwdb', req.body.dbName + '.json');
         
+           res.resObj = {
+                success : false,
+                mess: 'default message',
+                mode : req.query.m || 'post', // for a post or keyword
+                post: req.query.p || false,
+                keyword: req.query.k || false
+            };
         
-        res.json(res.resObj);
+            //res.json(res.resObj);
+            next();
  
-    });
+        },
+        
+        // check resObj
+        (req, res, next) => {
+            
+            let r = res.resObj;
+            
+            if(r.keyword){
+                
+                if((r.mode === 'post' && r.post) || r.mode === 'keyword'){
+                    
+                    next();
+                    
+                }else{
+                    
+                    r.mess = 'keyword given but in post mode and no post if given';
+                    res.json(r);
+                    
+                }
+                
+            }else{
+            
+                r.mess = 'no keyword given or no post given if in post mode';
+                
+                res.json(r);
+                
+            }
+            
+        },
+        
+        // send resObj
+        (req,res) =>{
+            
+            res.json(res.resObj);
+            
+        }
+        
+        
+    ]);
 
     // return the router to be used
     // in main server.js file
