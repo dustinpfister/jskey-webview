d64a9051f83f54817312a51951a073788e3ffc57
dustin pfister <dustin.pfister@gmail.com>
2019-08-14 09:52:08 -0400

    newe kwdb_index middleware started

---
+++ commands/server_middleware/kwdb_index/index.js
@@ -0,0 +1,32 @@
+// build index lists for posts and keywords
+let spawn = require('child_process').spawn,
+path = require('path'),
+express = require('express'),
+router = new express.Router();
+
+router.use(require('body-parser').json());
+
+module.exports = function(){
+    
+    router.post('/', (req, res, next) => {
+        
+            let dir_target = req.app.get('dir_target'),
+            path_kwdb = path.join(dir_target, '_kwdb', req.body.dbName + '.json');
+                
+            let resObj = {
+                success : false,
+                mess: 'default message'
+            };
+            
+            res.json(resObj);
+ 
+            
+        }
+    );
+
+    // return the router to be used
+    // in main server.js file
+    return router;
+    
+};
+
