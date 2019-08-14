9606962b4e7e8ead5017b30b9fb282c6cf0a4852
dustin pfister <dustin.pfister@gmail.com>
2019-08-14 09:55:52 -0400

    added kwdb_index to server.js

--- commands/server_middleware/kwdb_index/index.js
+++ commands/server_middleware/kwdb_index/index.js
@@ -8,7 +8,7 @@ router.use(require('body-parser').json());
 
 module.exports = function(){
     
-    router.post('/', (req, res, next) => {
+    router.get('/', (req, res, next) => {
         
             let dir_target = req.app.get('dir_target'),
             path_kwdb = path.join(dir_target, '_kwdb', req.body.dbName + '.json');
